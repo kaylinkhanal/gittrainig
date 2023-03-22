190d44f9fac30612276a9f736a23936fe54d7bc9
samir bajracharya <febsec14@gmail.com>
2023-03-22 09:23:03 +0545

    revert

--- samir.js
+++ samir.js
@@ -1,14 +1,12 @@
 const obj={
-    name:"Dinesh Mahatara Chhetri",
+    name:"sameer",
     address:{
-        street:"Shantitol",
+        street:"godawari",
         ward:14,
     }
 }
 
 console.log(`My name is ${obj.name}. I live in ${obj.address.street}.`)
-<<<<<<< HEAD
-console.log(`My Second name is ${obj.name}. I live in ${obj.address.street}.`)
-=======
+console.log(`My Second name is ${obj.name}. I live in ${obj.address.street}.`)=======
 console.log("Hello")
->>>>>>> 1ca8b029fd6ccb9fcf158e3c184ad8194b6939be
+
