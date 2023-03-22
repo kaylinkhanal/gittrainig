8c48f0863d7444e28c2f61ee9cd8d02d3c88249c
Shades45 <linkerizan99@gmail.com>
2023-03-22 09:05:41 +0545

    understanding the closure scope

--- bijan.js
+++ bijan.js
@@ -3,3 +3,19 @@ const secondarr = [4,5,6]
 
 const thridarr = firstarr.concat(secondarr)
 console.log(thridarr)
+
+var a  =10
+let b = 20
+function outer(){
+    let c = 30
+    function myValue(){
+        return 100
+    }
+    function inner(){
+        const d = 40
+        debugger;
+        return a+ b +c + d + myValue()
+    }
+    return inner()
+}
+outer()
