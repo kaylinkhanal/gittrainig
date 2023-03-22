1059deabfdfdac00fa42131bc4747ee2a660ae92
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2023-03-22 09:08:22 +0545

    Made chaanges in secondArray

--- rupa.js
+++ rupa.js
@@ -1,6 +1,7 @@
 const firstArr =[32,53,32]
-const secondArr=[1, 2 , 99]
+const secondArr=[1, 22 , 99]
  
 const output = firstArr.concat(secondArr)
 console.log(output)
-console.log("Hello I had made the some changes")
+console.log("Hello I had made the some changes");
+console.log("Raunak have made changes in second array");
