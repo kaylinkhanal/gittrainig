aada70f8773cbe64d942197744320a4819eb71af
Dinesh Mc <dineshmc20@gmail.com>
2023-03-22 09:23:41 +0545

    oke

--- samir.js
+++ samir.js
@@ -7,6 +7,4 @@ const obj={
 }
 
 console.log(`My name is ${obj.name}. I live in ${obj.address.street}.`)
-console.log(`My Second name is ${obj.name}. I live in ${obj.address.street}.`)=======
-console.log("Hello")
 
