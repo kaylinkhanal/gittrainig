47c2c2ea45892396383efe71297a75856ec6781b
Dinesh Mc <dineshmc20@gmail.com>
2023-03-22 09:20:37 +0545

    adding informamtion

--- samir.js
+++ samir.js
@@ -1,9 +1,10 @@
 const obj={
-    name:"sameer",
+    name:"Dinesh Mahatara",
     address:{
-        street:"godawari",
+        street:"Shantitol",
         ward:14,
     }
 }
 
 console.log(`My name is ${obj.name}. I live in ${obj.address.street}.`)
+console.log(`My Second name is ${obj.name}. I live in ${obj.address.street}.`)
